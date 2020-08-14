35baed180585cedc0109b106cc8b0c952e261a6c
Kamal Lama <kamal09rumba@gmail.com>
2020-08-14 19:53:52 +0545

    Migrate project   - [x] manage modules via composer   - [x] update drupal version   - [x] migrate custom theme and modules

---
+++ web/modules/custom/cccm_api_module/src/Controller/CCCMAPIController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Drupal\cccm_api\Controller;
+
+class CCCMAPIController {
+        public function CCCM() {
+        	return array (
+        		'#title' => 'CCCM API', 
+        		'#markup' => 'This is some content.', 
+        		);
+        }
+}
+?>
